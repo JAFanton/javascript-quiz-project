76a3f172956c495b36f299f9efbfe3d87ba64d92
Moaz Mubaydin <mmubaydin@gmail.com>
2024-10-07 15:31:36 +0200

    Finished Day 1

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,44 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+     getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+     }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        this.currentQuestionIndex++;
+    }
 
-    // 4. shuffleQuestions()
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions(){
+        for(let i = this.questions.length -1; i >= 0; i--){
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;
+            
+        }
+    }
 
-    // 6. hasEnded()
+
+    checkAnswer(answer){
+        if(answer === this.questions[this.currentQuestionIndex].answer){
+            this.correctAnswers++;
+        }
+    }
+
+    
+     hasEnded(){
+        if(this.questions.length -1 >= this.currentQuestionIndex){
+            return false;
+        }
+        return true;
+     }
 }
+
