8f94d0f82ef5965b0d03073890025518b0bb3f41
Moaz Mubaydin <mmubaydin@gmail.com>
2024-10-08 15:14:02 +0200

    Finished Day 2

--- src/quiz.js
+++ src/quiz.js
@@ -40,5 +40,24 @@ class Quiz {
         }
         return true;
      }
-}
 
+     filterQuestionByDifficulty(difficulty){
+
+        if(difficulty >= 1 && difficulty <= 3){
+            this.questions = this.questions.filter((question)=>{
+                return (question.difficulty === difficulty);
+   
+            })
+
+        }
+     }
+
+     averageDifficulty(){
+
+        const totalDifficulty = this.questions.reduce(function(acc, currentValue){
+            return acc + currentValue;
+        }, 0)
+
+        return totalDifficulty/this.questions.length;
+     }
+}
